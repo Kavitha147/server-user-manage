6d50afdb5f4d213956db7dbb6e7dc5cbe54daab8
Kavitha147 <kavimk471@gmail.com>
2022-07-19 10:54:34 +0530

    db model changes

--- dbmodel.js
+++ dbmodel.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 const dbData = new mongoose.Schema({
     userName: { type: 'string' },
     email: { type: 'string', unique: true },
-    password: { type: 'string' },
-    isAdmin: { type: 'boolean' }
+    password: { type: 'string' }
 });
 
 module.exports = mongoose.model("dbData", dbData);
